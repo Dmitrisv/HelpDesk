9170e2e064a21b71adaaac59804a29e21db60575
Dmitrisv <foxdog7777@gmail.com>
2023-03-09 21:42:59 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 python3 ./manage.py makemigrations requesting
 python3 ./manage.py migrate 
 mkdir staticfiles
-python3 ./manage.py collectstatic
+python3 ./manage.py collectstatic --noinput --clear
 
 exec gunicorn --config python:gunicorn_config main.asgi:application
